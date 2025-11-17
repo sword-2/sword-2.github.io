cd718ebccf43b51b1c8eaf328a840a7fd09ea76c
gregs650@gmail.com <gregs650@gmail.com>
2025-11-17 13:29:22 -0800

    fix error in json output

--- hvacFaith/04-thermostat/19-mkJsonFile.js
+++ hvacFaith/04-thermostat/19-mkJsonFile.js
@@ -1,12 +1,20 @@
 //make a json file the operator could copy out
+
+function mkJsonFile() {
+	let formattedOut = JSON.stringify(tablesJson);
+	return formattedOut;
+}
+
+/*
 function mkJsonFile() {
 	try {
-		let formattedOut = '';
+		let formattedOut = '{<br>';
 		for (let i = 0; i < tablesJson.length; i++) {
 			let tmpOut = '<br>{';
 			//tmpOut += '<br><br>{ "table": ' + cap + '},';
 			//tmpOut += '<br><br>{ "thermostat": ' + thermostat + '},';
 			tmpOut += '<br>' + JSON.stringify(tablesJson[i][0], null, 2);
+			tmpOut += ',';
 			for (let k = 1; k < tablesJson[i].length; k++) {
 				if (k == tablesJson[i].length - 1) {
 					tmpOut += JSON.stringify(tablesJson[i][k], null, 2);
@@ -14,11 +22,17 @@ function mkJsonFile() {
 					tmpOut += '<br><br>' + JSON.stringify(tablesJson[i][k], null, 2) + ',';
 				}
 			}
-			tmpOut += '<br><br>}';
+			if (i == tablesJson.length - 1) {
+				tmpOut += '<br><br>}';
+			} else {
+				tmpOut += '<br><br>},';
+			}
 			formattedOut += tmpOut;
 		}
+		formattedOut += '<br>}';
 		return formattedOut;
 	} catch (error) {
 		alert("mkJsonFile() error: " + error.message);
 	}
 } //end function
+*/
